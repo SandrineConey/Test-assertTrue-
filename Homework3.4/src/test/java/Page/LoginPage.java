14cb206e5aa2350938464875b28f8acfd56db8f4
Alexandr <sasha_govako@mail.ru>
2024-05-08 21:02:26 +0300

    Removing drivers

--- Homework3.4/src/test/java/Page/LoginPage.java
+++ Homework3.4/src/test/java/Page/LoginPage.java
@@ -4,16 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 public class LoginPage {
-    static WebDriver driver = new ChromeDriver();
+
     private By inputLogin = By.xpath("//div[@class='it_w']//input[@name='st.email']");
     private By inputPassword = By.xpath("//div[@class='it_w']//input[@name='st.password']");
     private By button = By.xpath("//div[@class='login-form-actions']//input[@class='button-pro __wide']");
 
 
     public void input(String login, String password){
-        driver.findElement(inputLogin).sendKeys(login);
-        driver.findElement(inputPassword).sendKeys(password);
-        driver.findElement(button).click();
+        $(inputLogin).shouldBe(visible).setValue(login);
+        $(inputPassword).shouldBe(visible).setValue(password);
+        $(button).shouldBe(visible).click();
     }
 }
--- Homework3.4/src/test/java/Test/BeforeTest.java
+++ Homework3.4/src/test/java/Test/BeforeTest.java
@@ -1,24 +1,26 @@
 package Test;
 
 import Page.LoginPage;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class BeforeTest {
     private static String login = "technopol47";
     private static String password = "technopolisPassword";
-    static String url = "https://ok.ru/";
-
-    static WebDriver driver = new ChromeDriver();
 
     @BeforeAll
     public static void open(){
-        driver.get(url);
-        driver.manage().window().maximize();
+        Configuration.browser = CHROME;
+        Configuration.baseUrl = "https://ok.ru/";
+        Configuration.browserSize = "1920x1080";
+        Selenide.open("/");
     }
     public static void authorize() {
         LoginPage loginPage = new LoginPage();
